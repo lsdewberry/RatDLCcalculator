df1170ed2445fd19a0698004ef9af07d8380ae05
lsdewberry <90293589+lsdewberry@users.noreply.github.com>
2022-01-27 12:44:01 -0500

    updated and fixed a bunch of stuff
    the visualization codes don't work and I use R so I'm getting rid of them next time

--- normalize_and_format_for_excel.m
+++ normalize_and_format_for_excel.m
@@ -2,44 +2,30 @@
 for i=1:length(output)
     try
     output(i).velocity =  ( abs(gradient(output(i).N.x.avg)) ./ output(i).pixpercm ) ./ (gradient(output(i).N.f.avg) ./500);
-    output(i).velocityB =  ( abs(gradient(output(i).B.x.avg)) ./ output(i).pixpercm ) ./ (gradient(output(i).B.f.avg) ./500);
+    %output(i).velocityB =  ( abs(gradient(output(i).B.x.avg)) ./ output(i).pixpercm ) ./ (gradient(output(i).B.f.avg) ./500);
     output(i).velocitystd = std(output(i).velocity);
     output(i).avgV = mean(output(i).velocity);
-    
-    output(i).Hstridelength=output(i).Hstridelength.avg/output(i).pixpercm;
+    maxHTy(i) = max(output(i).HT.y.avg);
+    %output(i).Hstridelength=output(i).Hstridelength.avg/output(i).pixpercm;
     end
 end
-
+%% velocity
 %VARx table
 for i=1:length(output)
     try
-    mylength(i)=length(output(i).HT.x.avg);
+    mylength(i)=length(output(i).velocity);
     end
 end
 mylength=max(mylength);
 varx=nan(length(output),mylength);
 for i=1:length(output)
     try
-    myvel=output(i).HT.x.avg;
-    varx(i,1:length(myvel))=myvel/output(i).pixpercm;
+    myvel=output(i).velocity;
+    varx(i,1:length(myvel))=myvel;
     end
 end
-    
-%VARy table
-for i=1:length(output)
-    try
-    mylength(i)=length(output(i).HT.y.avg);
-    end
-end
-mylength=max(mylength);
-vary=nan(length(output),mylength);
-for i=1:length(output)
-    try
-    myvel=output(i).HT.y.avg;
-    vary(i,1:length(myvel))=myvel/output(i).pixpercm;
-    end
-end
-
+filename = 'gait3_1.xlsx';
+writematrix(varx,filename,'Sheet',2)    
 %VARf table
 for i=1:length(output)
     try
@@ -54,3 +40,52 @@ for i=1:length(output)
     varf(i,1:length(myvel))=myvel;
     end
 end
+writematrix(varf,filename,'Sheet',3)  
+%% var of interest
+%VARx table
+for i=1:length(output)
+    try
+    mylength(i)=length(output(i).HT.x.avg);
+    end
+end
+mylength=max(mylength);
+varx=nan(length(output),mylength);
+for i=1:length(output)
+    try
+    myvel=output(i).HT.x.avg;
+    varx(i,1:length(myvel))=myvel/output(i).pixpercm;
+    end
+end
+writematrix(varx,filename,'Sheet',4)  
+
+%VARy table
+for i=1:length(output)
+    try
+    mylength(i)=length(output(i).HT.y.avg);
+    end
+end
+mylength=max(mylength);
+vary=nan(length(output),mylength);
+for i=1:length(output)
+    try
+    myvel=output(i).HT.y.avg;
+    vary(i,1:length(myvel))=myvel/output(i).pixpercm;
+    end
+end
+writematrix(vary,filename,'Sheet',5)
+
+%VARf table
+for i=1:length(output)
+    try
+    mylength(i)=length(output(i).HT.f.avg);
+    end
+end
+mylength=max(mylength);
+varf=nan(length(output),mylength);
+for i=1:length(output)
+    try
+    myvel=output(i).HT.f.avg;
+    varf(i,1:length(myvel))=myvel;
+    end
+end
+writematrix(varf,filename,'Sheet',6)  
